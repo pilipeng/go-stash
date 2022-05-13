e0b9c2443e15989f9b62a6ff3da5426315577b0a
霹靂砰 <xg.h@qq.com>
2022-05-13 20:37:36 +0800

    format

--- stash/dal/common_test.go
+++ stash/dal/common_test.go
@@ -5,9 +5,8 @@ import (
 )
 
 func TestTable2index(t *testing.T) {
-	tableName := "fx_order"
-	var createTime int64
-	createTime = 1632272916
+	tableName := "pn_order"
+	createTime := 1632272916
 	indexName := Table2index("100006_", tableName, createTime)
 	if indexName != "100006_fx_order_202109" {
 		t.Fatalf("indexName:%s", indexName)
--- stash/dal/goods.go
+++ stash/dal/goods.go
@@ -2,38 +2,39 @@ package dal
 
 import (
 	"context"
+	"time"
+
 	"github.com/kevwan/go-stash/stash/model"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
-	"time"
 )
 
-func GoodsFormat(ctx context.Context,conn sqlx.SqlConn, tableData model.PnGoods,spuCityCodes map[int64][]int64) map[string]interface{} {
+func GoodsFormat(ctx context.Context, conn sqlx.SqlConn, tableData model.PnGoods, spuCityCodes map[int64][]int64) map[string]interface{} {
 	esMap := make(map[string]interface{})
 
 	esMap["index_name"] = "fx_goods"
-	esMap["add_time"] =time.Now().UnixNano() / 1e6
+	esMap["add_time"] = time.Now().UnixNano() / 1e6
 	esMap["brand_id"] = tableData.BrandId
 	esMap["cat_id"] = tableData.CatId
 	esMap["flavor_id"] = 0
 	esMap["flavor_name"] = ""
 	if tableData.CatId == 26 {
-		cfm, err := model.NewPnCakeFlavorModel(conn).FindOne(ctx,tableData.SpuId)
+		cfm, err := model.NewPnCakeFlavorModel(conn).FindOne(ctx, tableData.SpuId)
 		if err != nil {
-			logx.Errorf("no find spu_id:%s in fx_cakeFlavor ", tableData.SpuId)
+			logx.Errorf("no find spu_id:%d in fx_cakeFlavor ", tableData.SpuId)
 		}
 		if cfm != nil {
 			esMap["flavor_id"] = cfm.FlavorId
 			esMap["flavor_name"] = cfm.FlavorName
-		}else {
+		} else {
 			esMap["flavor_id"] = 0
 			esMap["flavor_name"] = ""
 		}
 	}
 	//esMap["city_code"] = GoodsCityCodes(conn, tableData)
-	if cityCodes,ok:=spuCityCodes[tableData.SpuId];ok {
+	if cityCodes, ok := spuCityCodes[tableData.SpuId]; ok {
 		esMap["city_code"] = cityCodes
-	}else{
+	} else {
 		esMap["city_code"] = []int{}
 	}
 
@@ -67,17 +68,16 @@ func GoodsFormat(ctx context.Context,conn sqlx.SqlConn, tableData model.PnGoods,
 	return esMap
 }
 
-
-func GoodsCityCodes(ctx context.Context,conn sqlx.SqlConn, goodsSpuIds []int64) map[int64][]int64 {
-	list, err := model.NewPnGoodsCityModel(conn).FindAllBySpuIds(ctx,goodsSpuIds)
+func GoodsCityCodes(ctx context.Context, conn sqlx.SqlConn, goodsSpuIds []int64) map[int64][]int64 {
+	list, err := model.NewPnGoodsCityModel(conn).FindAllBySpuIds(ctx, goodsSpuIds)
 	if err != nil {
 		logx.Must(err)
 	}
 
-	spuCityCodes:=make(map[int64][]int64, len(goodsSpuIds))
+	spuCityCodes := make(map[int64][]int64, len(goodsSpuIds))
 
 	for _, cityInfo := range *list {
-		spuCityCodes[cityInfo.SpuId]= append(spuCityCodes[cityInfo.SpuId],cityInfo.CityCode)
+		spuCityCodes[cityInfo.SpuId] = append(spuCityCodes[cityInfo.SpuId], cityInfo.CityCode)
 	}
 	//lib.StructColumn(&cityIdList, *list, "CityCode", "")
 	return spuCityCodes
