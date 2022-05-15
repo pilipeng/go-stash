0958b8beb1b910ae6ef86d71075f418e9ea2809c
霹靂砰 <xg.h@qq.com>
2022-05-15 10:41:32 +0800

    go test up

--- stash/dal/common_test.go
+++ stash/dal/common_test.go
@@ -6,9 +6,10 @@ import (
 
 func TestTable2index(t *testing.T) {
 	tableName := "pn_order"
-	createTime := 1632272916
-	indexName := Table2index("100006_", tableName, createTime)
-	if indexName != "100006_fx_order_202109" {
+	var createTime int64
+	createTime = 1632272916
+	indexName := Table2index("go_by_", tableName, createTime)
+	if indexName != "go_by_order_202109" {
 		t.Fatalf("indexName:%s", indexName)
 	}
 }
